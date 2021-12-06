50516f92c9094842f0bbbdd883daa40898d96571
iamfromgermany <76224662+iamfromgermany@users.noreply.github.com>
2021-12-06 20:01:42 +0100

    Update School-Catalogue.js
    Added Code

--- Code-Academy/School-Catalogue.js
+++ Code-Academy/School-Catalogue.js
@@ -8,3 +8,80 @@ wants the catalog to hold quick reference material for each school in the city.
 
 
 */ 
+
+class School {
+  constructor (name, level, numbersOfStudents){
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numbersOfStudents;
+  }
+
+  get name(){
+    return this._name; 
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numbersOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numbersOfStudents(value) {
+    if (isNaN(value)) {
+      console.log("Invalid input: numberOfStudents must be set to a Number.")
+    } else {
+      this._numberOfStudents = value;
+    }
+  }
+
+  quickFacts(){
+    console.log(`${this.name} educates ${this.numbersOfStudents} students at the ${this.level} school level.`)
+  }
+
+  static pickSubstituteTeacher(substituteTeachers){
+    let randomIndex = Math.floor(Math.random() * substituteTeachers.length)
+    return substituteTeachers[randomIndex]
+  }
+}
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy() {
+    return this._pickupPolicy
+  }
+}
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, "high", numberOfStudents);
+    this._sportsTeam = sportsTeams;
+  }
+
+  get sportsTeams() {
+    for (let sports in sportsTeams) {
+      console.log(sports);
+    }
+  }
+}
+
+class MiddleSchool extends School {
+  
+}
+
+
+//Tests
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+
+console.log(lorraineHansbury)
+console.log(lorraineHansbury.quickFacts())
+console.log (School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']))
+
+alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+
+console.log(alSmith)
