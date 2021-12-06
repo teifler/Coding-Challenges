9de296fd3cace49c53810fe6629a2618383e4cba
iamfromgermany <76224662+iamfromgermany@users.noreply.github.com>
2021-12-06 20:01:16 +0100

    Added new Challenge
    Codec Ademy - School Catalogue
    
    Link:
    https://www.codecademy.com/paths/front-end-engineer-career-path/tracks/fecp-javascript-syntax-part-iii/modules/fecp-learn-javascript-syntax-classes/projects/school-catalog

---
+++ Code-Academy/School-Catalogue.js
@@ -0,0 +1,10 @@
+/*
+https://www.codecademy.com/paths/front-end-engineer-career-path/tracks/fecp-javascript-syntax-part-iii/modules/fecp-learn-javascript-syntax-classes/projects/school-catalog
+School Catalogue
+
+Let’s put your knowledge of classes to the test by creating a digital school 
+catalog for the New York City Department of Education. The Department of Education 
+wants the catalog to hold quick reference material for each school in the city.
+
+
+*/ 
