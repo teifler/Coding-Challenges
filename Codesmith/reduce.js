b209ba110bb57bc4e53ae899495734bf9fbd3e1b
teifler <tim.eif@protonmail.com>
2021-12-21 12:22:49 +0100

    Update reduce.js
    Improved the solution

--- Codesmith/reduce.js
+++ Codesmith/reduce.js
@@ -3,13 +3,25 @@
 
 // ADD CODE HERE
 const reduce = (arr, callback1, iniVal) => {
-  
-  for (let i=0; i < arr.length; i++) {
+  if (Array.isArray(arr)) {
+    let acc;
+    if (iniVal === undefined) {
+      acc = arr[0];
+      arr.shift();
+    } else {
+      acc = iniVal;
+      
+    }
+    for (let i=0; i < arr.length; i++) {
     iniVal= callback1(iniVal, arr[i])
+  	}
+  	return iniVal
+  } else {
+    console.log("First Element should be an array")
   }
-  return iniVal
+  
 }
 // Uncomment these to check your work!
  const nums = [4, 1, 3];
  const add = function(a, b) { return a + b; }
- console.log(reduce(nums, add, 5)); // should log 8
+ console.log(reduce(nums, add, 0)); // should log 8
