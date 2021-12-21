08c26f3eafe7ea24900a38cdd5851e174af5bfd3
teifler <tim.eif@protonmail.com>
2021-12-21 12:07:27 +0100

    Solution
    Solved the Challenge

--- Codesmith/reduce.js
+++ Codesmith/reduce.js
@@ -2,8 +2,14 @@
 //https://csx.codesmith.io/units/callbacks/challenge-reduce
 
 // ADD CODE HERE
-
+const reduce = (arr, callback1, iniVal) => {
+  
+  for (let i=0; i < arr.length; i++) {
+    iniVal= callback1(iniVal, arr[i])
+  }
+  return iniVal
+}
 // Uncomment these to check your work!
-// const nums = [4, 1, 3];
-// const add = function(a, b) { return a + b; }
-// console.log(reduce(nums, add, 0)); // should log 8
+ const nums = [4, 1, 3];
+ const add = function(a, b) { return a + b; }
+ console.log(reduce(nums, add, 5)); // should log 8
