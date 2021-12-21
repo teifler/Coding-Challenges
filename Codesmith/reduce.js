1a0e0941354ca9f3b9b22e332736f913df8a7d7f
teifler <tim.eif@protonmail.com>
2021-12-21 12:04:16 +0100

    Added Challenges

---
+++ Codesmith/reduce.js
@@ -0,0 +1,9 @@
+//Challenge: reduce
+//https://csx.codesmith.io/units/callbacks/challenge-reduce
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const nums = [4, 1, 3];
+// const add = function(a, b) { return a + b; }
+// console.log(reduce(nums, add, 0)); // should log 8
